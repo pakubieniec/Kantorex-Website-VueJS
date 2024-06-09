32336118185611463668a347cc9f7ee60b10b9f2
pakubieniec <pakubieniec@gmail.com>
2024-06-09 23:02:52 +0200

    Second commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/Kantorex-Website-VueJS/" : "/",
   plugins: [
     vue(),
   ],
@@ -14,3 +15,5 @@ export default defineConfig({
     }
   }
 })
+
+
