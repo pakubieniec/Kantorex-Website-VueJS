3bdaf47774b9b7358aa060b21d3500f3cdb128b5
pakubieniec <pakubieniec@gmail.com>
2024-06-10 10:10:31 +0200

    checking connetct with api

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HomeView from './views/HomeView.vue';
+const fetcher = fetch('http://api.nbp.pl/api/exchangerates/tables/a/')
+  .then(res => res.json())
+  .then(data => this.array = data)
+  .catch(err => console.log(err.message))
+
+  preload('/api/user', fetcher)
 </script>
 
 <template>
